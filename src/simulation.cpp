dc46625b4c49057013bd1cdceae8450ae8f8ffa9
vkrukhmalev <Krukhmalev02@gmail.com>
2024-07-28 02:04:22 -0400

    Added water

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -11,14 +11,21 @@ void Simulation::Start() {
 	
 }
 
-
-
 void Simulation::Update(float dt) {
 	brush_.Update();
+	bool dir = false;
 	for (int i = 0; i < height_; ++i) {
-		for (int j = 0; j < width_; ++j) {
-			matrix_.Update(j, i);
+		if (dir) {
+			for (int j = 0; j < width_; ++j) {
+				matrix_.Update(j, i);
+			}
 		}
+		else {
+			for (int j = width_ - 1; j >= 0; --j) {
+				matrix_.Update(j, i);
+			}
+		}
+		dir = !dir;
 	}
 	matrix_.FlipUpdateFlag();
 }
